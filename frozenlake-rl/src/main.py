d45e3dd7cd332fba9e2ef2d16cc8062f32fbcc0e
Francis Curodeau <franciscurodeau@gmail.com>
2026-03-21 16:07:12 -0400

    Création d'un gabarit avec 3 cas d'étude

--- frozenlake-rl/src/main.py
+++ frozenlake-rl/src/main.py
@@ -1,6 +1,98 @@
 import gymnasium as gym
 
-env = gym.make("FrozenLake-v1", render_mode="human", map_name="4x4")
+cas_etude = {
+    "easy": {
+        "desc": [
+            "SFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFG",
+        ],
+        "is_slippery": True,
+        "success_rate": 0.9,
+        "reward_schedule": (1, 0, 0),
+    },
+    "medium": {
+        "desc": [
+            "SFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFG",
+        ],
+        "is_slippery": True,
+        "success_rate": 0.7,
+        "reward_schedule": (1, 0, 0),
+    },
+    "hard": {
+        "desc": [
+            "SFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFF",
+            "FFFFFFFFFFFFFFFFFFFG",
+        ],
+        "is_slippery": True,
+        "success_rate": 0.5,
+        "reward_schedule": (1, 0, 0),
+    },
+}
+
+config = cas_etude["medium"]
+
+env = gym.make(
+    "FrozenLake-v1",
+    desc=config ["desc"],
+    is_slippery=config ["is_slippery"],
+    success_rate=config ["success_rate"],
+    reward_schedule=config ["reward_schedule"],
+)
 
 state, info = env.reset()
 
