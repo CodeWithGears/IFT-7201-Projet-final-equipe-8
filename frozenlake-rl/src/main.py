186ae8e63e8de2b85a4e443288a3d902d4bc4fa9
Francis Curodeau <franciscurodeau@gmail.com>
2026-03-21 16:58:12 -0400

    Ajout des fonction pour entrainement DQN et PPO

--- frozenlake-rl/src/main.py
+++ frozenlake-rl/src/main.py
@@ -1,4 +1,6 @@
+import os
 import gymnasium as gym
+from stable_baselines3 import DQN, PPO
 
 cas_etude = {
     "easy": {
@@ -84,24 +86,59 @@ cas_etude = {
     },
 }
 
-config = cas_etude["medium"]
+def DQN_training (env, retrain=False, name="dqn_FrozenLake"):
+    if retrain or not os.path.exists(f"{name}.zip"):
+        model = DQN("MlpPolicy", env, verbose=1)
+        model.learn(total_timesteps=10000, log_interval=4)
+        model.save(name)
+    else:
+        model = DQN.load(name, env=env)
+    return model
 
-env = gym.make(
-    "FrozenLake-v1",
-    desc=config ["desc"],
-    is_slippery=config ["is_slippery"],
-    success_rate=config ["success_rate"],
-    reward_schedule=config ["reward_schedule"],
-)
+def PPO_training (env, retrain=False, name="ppo_FrozenLake"):
+    if retrain or not os.path.exists(f"{name}.zip"):
+        model = PPO("MlpPolicy", env, verbose=1)
+        model.learn(total_timesteps=10000, log_interval=4)
+        model.save(name)
+    else:
+        model = PPO.load(name, env=env)
+    return model
 
-state, info = env.reset()
+def main():
+    #Entrainer/charger les modèles DQN et PPO pour chaque cas d'étude
+    DQN_models = {}
+    PPO_models = {}
+    train_models = False
+    for cas in cas_etude.keys():
+        config = cas_etude[cas]
 
-done = False
+        env = gym.make(
+            "FrozenLake-v1",
+            desc=config ["desc"],
+            is_slippery=config ["is_slippery"],
+            success_rate=config ["success_rate"],
+            reward_schedule=config ["reward_schedule"],
+            render_mode="human",
+        )
+        print(f"Training DQN for {cas} case...")
+        DQN_models[cas] = DQN_training(env, retrain=train_models, name=f"dqn_FrozenLake_{cas}")
 
-while not done:
-    action = env.action_space.sample()
-    state, reward, terminated, truncated, info = env.step(action)
-    print("State:", state, "Reward:", reward)
-    done = terminated or truncated
+        print(f"Training PPO for {cas} case...")
+        PPO_models[cas] = PPO_training(env, retrain=train_models, name=f"ppo_FrozenLake_{cas}")
 
-env.close()
+    #Évaluation des modèles DQN et PPO pour chaque cas d'étude
+
+    state, _ = env.reset()
+
+    terminated, truncated = False, False
+
+    while not (terminated or truncated):
+        action = env.action_space.sample()
+        state, reward, terminated, truncated, _ = env.step(action)
+        print("State:", state, "Reward:", reward)
+        done = terminated or truncated
+
+    env.close()
+
+if __name__ == "__main__":
+    main()
